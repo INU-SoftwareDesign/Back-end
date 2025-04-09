1d13c595d67dba9ac27abc00794ca70aed81ceeb
asdas132 <fktmxm1320@gmail.com>
2025-04-10 02:02:31 +0900

    성적 관리 API 구현

--- grades/urls.py
+++ grades/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.GradeView.as_view()),
-    #path('<int:pk>/', views.GradeDetailView.as_view()),
+    path('', views.GradeView.as_view()),
+    path('<int:pk>/', views.GradeDetailView.as_view()),
 ]
