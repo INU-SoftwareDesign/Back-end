64d9a6e6bd7ef4cd14d8a11b2f88523a45d3ab9e
asdas132 <fktmxm1320@gmail.com>
2025-04-12 01:54:12 +0900

    피드백 관리 API 구현

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.FeedbackView.as_view()),
-    #path('<int:pk>/', views.FeedbackDetailView.as_view()),
+    path('', views.FeedbackView.as_view()),
+    path('<int:pk>/', views.FeedbackDetailView.as_view()),
 ]
