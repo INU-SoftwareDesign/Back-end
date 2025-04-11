70705bead42e34aef6a6544f463d699f9a9f899b
asdas132 <fktmxm1320@gmail.com>
2025-04-12 03:07:29 +0900

    학생 관리 json 수정에 따른 재구현

--- students/serializers.py
+++ students/serializers.py
@@ -3,40 +3,33 @@ from .models import Student
 from accounts.models import User
 from classrooms.models import Classroom
 
+class UserSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'name']
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'name', 'email', 'phone']
+
 class ClassroomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Classroom
         fields = ['id', 'grade', 'class_number']
 
-class UserSerializer(serializers.ModelSerializer):
+class StudentListSerializer(serializers.ModelSerializer):
+    user = UserSimpleSerializer(read_only=True)
+    classroom = ClassroomSerializer(read_only=True)
+
     class Meta:
-        model = User
-        fields = ['id', 'username', 'name', 'email', 'phone']
+        model = Student
+        fields = ['id', 'user', 'student_number', 'classroom']
 
 class StudentDetailSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    classroom = ClassroomSerializer()
+    user = UserDetailSerializer(read_only=True)
+    classroom = ClassroomSerializer(read_only=True)
 
     class Meta:
         model = Student
         fields = ['id', 'user', 'student_number', 'special_note', 'classroom']
-
-    def update(self, instance, validated_data):
-        classroom_data = validated_data.pop('classroom', None)
-        if classroom_data:
-            classroom = Classroom.objects.filter(
-                grade=classroom_data['grade'],
-                class_number=classroom_data['class_number']
-            ).first()
-            if classroom:
-                instance.classroom = classroom
-        return super().update(instance, validated_data)
-
-class StudentListSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source='user.name', read_only=True)
-    classroom = ClassroomSerializer()
-
-    class Meta:
-        model = Student
-        fields = ['id', 'name', 'student_number', 'classroom']
-
--- students/views.py
+++ students/views.py
@@ -2,8 +2,6 @@ from rest_framework import generics
 from .models import Student
 from .serializers import StudentListSerializer, StudentDetailSerializer
 
-# Create your views here.
-
 class StudentView(generics.ListAPIView):
     queryset = Student.objects.select_related('user', 'classroom').all()
     serializer_class = StudentListSerializer
